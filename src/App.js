9026e22fb2a5e2c639ba0dcda1fc6aa732d09ac7
dongocduyen <dongocduyen113@gmail.com>
2022-12-01 20:08:39 +0700

    Format again

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import axios from "axios";
-import React, { Fragment, useState } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
-import GithubState from "./context/github/githubState";
 import Main from "./components/layout/Main";
 import Footer from "./components/layout/Footer";
+import GithubState from "./context/github/githubState";
 
 
 const App = () => {
@@ -15,7 +14,7 @@ const App = () => {
         <div>
           <Navbar />
           <Main />
-          {/* <Footer /> */}
+          <Footer />
         </div>
       </Router>
     </GithubState>
